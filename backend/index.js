e2617e46ce712bd4808c4974efc751e67d7af5d2
Sudhakar <sudhakarmishra2011@gmail.com>
2023-05-28 03:54:33 +0530

    Initial Checkin

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require("cors")
+require('./db/config');
+const User = require("./db/User");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", async (req,resp)=>{
+    let user = new User(req.body);
+    let result = await user.save();
+    resp.send(result);
+})
+
+app.listen(5000)
---
+++ front-end/src/components/SignUp.js
@@ -0,0 +1,34 @@
+import React,{useState} from "react";
+
+const SignUp = () =>{
+    const [name,setName] = useState("")
+    const [password,setPassword] = useState("")
+    const [email,setEmail] = useState("")
+    
+    const collectData = async () => {
+      console.log(name,email,password) 
+      
+      let result = await fetch('http://localhost:5000/register',{
+        method:'post',
+        body: JSON.stringify({name,email,password}),
+        headers:{
+            'Content-Type':'application/json'
+        }
+      })
+
+      result = await result.json()
+      console.log(result)
+    }
+    
+    return (
+        <div className="register">
+            <h1>Register</h1>
+            <input value={name} onChange={(e)=> setName(e.target.value)} className="inputBox" type="text" placeholder="Enter Name" />
+            <input value={email} onChange={(e)=> setEmail(e.target.value)} className="inputBox" type="text" placeholder="Enter Email" />
+            <input values={password} onChange={(e)=> setPassword(e.target.value)} className="inputBox" type="password" placeholder="Password" />
+            <button onClick={collectData} className="appButton">Sign Up</button>
+        </div>
+    )
+}
+
+export default SignUp;
