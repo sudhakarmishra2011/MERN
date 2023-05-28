9d6dd7fa1f040f4c49fa29b86572d9eacd180821
Sudhakar <sudhakarmishra2011@gmail.com>
2023-05-29 00:02:27 +0530

    Add Product API + Form Validation

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require("cors")
 require('./db/config');
 const User = require("./db/User");
+const Product = require('./db/Product')
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -27,5 +28,11 @@ app.post("/login", async (req, resp) => {
     }
 
 })
+
+app.post("/add-product", async (req,resp)=>{
+    let product = new Product(req.body)
+    let result = await product.save();
+    resp.send(result)
+});
 
 app.listen(5000)
